96e1b98b957bfd31d46fabcac0f59aefbcfeb110
ItaloRamirezA <113093861+ItaloRamirezA@users.noreply.github.com>
2024-12-11 21:14:52 +0100

    metodo agregarAlCarrito y style al boton eliminar #4

--- script.js
+++ script.js
@@ -16,17 +16,37 @@ let carrito = [];
  * Función para agregar productos al carrito
  */
 function agregarAlCarrito(index) {
+    let carrito = JSON.parse(localStorage.getItem('carrito')) || [];
+
     carrito.push(productos[index]);
 
-    // TODO
-}  
+    localStorage.setItem('carrito', JSON.stringify(carrito));
+
+    let carritoLista = document.getElementById('carrito-lista');
+    carritoLista.innerHTML = '';
+
+    // Se agrega al carrito
+    for (let i = 0; i < carrito.length; i++) {
+        let producto = carrito[i];
+        let li = document.createElement('li');
+        li.innerHTML =
+            ` ${producto.nombre} - €${producto.precio} 
+            <br><button class="boton-eliminar" onclick="eliminarProductoCarrito(${i})">Eliminar</button>`;
+        carritoLista.appendChild(li);
+    }
+}
 
 /**
  * Función para eliminar un producto del carrito
  */
-function eliminarProductoCarrito() {
-    carrito.splice(index, 1);
-    actualizarCarrito();
+function eliminarProductoCarrito(index) {
+    // Aqui debes coger el index(indice) y eliminar ese
+    // indice del array y del html, el indice va de 0 a 6 (mira html)
+
+    // El boton ya tiene como parametro el numero de
+    // indice de cada producto, solo haz que se elimine
+
+    // Hacer que el carrito sea responsive, que no ocupe toda la pantalla en movil
 }
 
 /**
@@ -34,5 +54,16 @@ function eliminarProductoCarrito() {
  * carrito y mostrar productos neuvos
  */
 function actualizarCarrito() {
+    // Igual tienes que cambiar algo de agregarAlCarrito
+    // para usar este metodo para que se muestre en el carrito
 
+    // hacer que en localStorage se elimine y guarde bien los productos
+}
+
+/**
+ * Aqui se carga
+ * todo del localStorage
+ */
+function onload() {
+    
 }
